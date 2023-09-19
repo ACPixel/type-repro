84b719e5e7a61741c440ce1db9356db537d474c1
ACPixel <acpixel@acpixel.com>
2023-09-19 12:30:40 -0700

    idk

--- app/routes/someroute.tsx
+++ app/routes/someroute.tsx
@@ -1,24 +1,18 @@
-import { json } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/cloudflare"; //types
+import { useLoaderData } from "@remix-run/react"; //types
 
 export default function Index() {
   const { badge } = useLoaderData<typeof loader>();
   if (badge) {
+    // ^?
     console.log(badge.ID);
     //                ^?
   }
   return <div className="text-black">{typeof badge.ID}</div>;
 }
 
-type Badge = {
-  ID: number;
-  Url: string;
-  Artist: string;
-  unlocked_after: number;
-};
-
 export async function loader() {
-  let badge: Badge = {
+  let badge = {
     ID: 1,
     Url: "a",
     Artist: "a",
